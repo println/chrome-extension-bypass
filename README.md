98183ea8fac487743e6caf4bb83a364bb6b36eb5
Felipe Santos <live.proto@hotmail.com>
2021-08-22 14:59:35 -0300

    Remove youtube and add cup*
    Signed-off-by: Felipe Santos <live.proto@hotmail.com>

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # chrome-extension-bypass
+
+## How use
+1. In chrome, access `Menu > More Tools > Extensions`
+2. Enable development mode, the switch is in top-left corner
+3. Clone this repository
+4. In chrome extensions, click in button `Load Unpacked`
+5. Select the folder of cloned project
+6. Enjoy
+
+## Explaining how this work
+1. This extension create a blue button on Test Viewer, `Extrair Prova`
+2. This button  open new tab with test only, with no menu and others
+3. In new tab, the test is clean to keep only the questions
+
--- main.js
+++ main.js
@@ -1,11 +1,38 @@
 'use strict';
 
-const TARGET = 'td.bodyData';
+function extract() {
+    var popup = window.open ('/', 'Test');
+    popup.close();
 
-if (document.location.href.includes("youtube.com")) {
-    let youtube = new Youtube(document.location.href);
-    youtube.startWithReplacement(TARGET);
-} else {
-    let radioCidade = new RadioCidade(document.location.href);
-    radioCidade.startWithReplacement(TARGET);
-} 
+    var url = document.getElementById("preview_frame").src;
+    popup = window.open (url, 'Test');
+
+    popup.onload = () => {
+        let popupQuestions = popup.document.getElementById("questions");
+        let popupWrapper = popup.document.getElementById("wrapper");
+        let popupMain = popup.document.getElementById("main");
+        popupWrapper.prepend(popupQuestions);
+        popupMain.remove();
+    }
+}
+
+function embedExtractButton() {
+    var elContainer = document.querySelector("#content>.submission-details-header");
+    var elDiv = document.createElement("div");
+    var elButton = document.createElement("button");
+
+    elDiv.className = "button-container";
+    elButton.className = "Button Button--primary";
+    elButton.setAttribute("type", "button");
+    elButton.onclick = () => extract();
+    elButton.textContent = "Extrair Prova";
+    elButton.id = "bypass-extractor";
+
+    elDiv.append(elButton);
+    elContainer.append(elDiv);
+}
+
+embedExtractButton();
+
+
+
