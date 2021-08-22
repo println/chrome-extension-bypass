09e77b1306989117ceac0e748d114ac421e19758
Felipe Santos <live.proto@hotmail.com>
2021-08-22 18:36:17 -0300

    Enables firefox compatibility

--- main.js
+++ main.js
@@ -1,19 +1,43 @@
 'use strict';
 
-function extract() {
-    var popup = window.open ('/', 'Test');
-    popup.close();
+function anonymizerTest(newWindow) {
+    let popupQuestions = newWindow.document.getElementById("questions");
+    let popupWrapper = newWindow.document.getElementById("wrapper");
+    let popupMain = newWindow.document.getElementById("main");
+    popupWrapper.prepend(popupQuestions);
+    popupMain.remove();
+}
 
-    var url = document.getElementById("preview_frame").src;
-    popup = window.open (url, 'Test');
-
-    popup.onload = () => {
-        let popupQuestions = popup.document.getElementById("questions");
-        let popupWrapper = popup.document.getElementById("wrapper");
-        let popupMain = popup.document.getElementById("main");
-        popupWrapper.prepend(popupQuestions);
-        popupMain.remove();
+function onAfterLoaded(newWindow) {
+    var isLoaded = false;
+    newWindow.onload = () => {
+        isLoaded = true;
+        anonymizerTest(newWindow);
     }
+
+    var times = 0;
+    var maxTimes = 10;
+    var interval = setInterval(() => {
+        if (isLoaded) {
+            window.console.log("Discard Compatibility...");
+            clearInterval(interval);
+        } else if (newWindow.document.readyState == "complete") {
+            window.console.log("Apply Compatibility...");
+            clearInterval(interval);
+            anonymizerTest(newWindow);
+        }
+
+        if (times++ >= maxTimes) {
+            window.console.log("Expires...");
+            clearInterval(interval);
+        }
+    }, 500);
+}
+
+function extract() {
+    var url = document.getElementById("preview_frame").src;
+    var newWindow = window.open(url, 'Test', null, true);
+    setTimeout(() => onAfterLoaded(newWindow), 1000);
 }
 
 function embedExtractButton() {
